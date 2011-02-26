066d41dc11485bda0d3ac080e08cad515659390f
Digvijay Lamba <digvijay@gmail.com>
2011-02-26 04:25:54 +0100

    Initial ruby project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lumlate::Application.config.secret_token = '8344b1b11b5b0f7a67ad08b1f93eba730ba7f156ab3a766958e10f8c2e698703d6dade8e872b899162b6f9dfe3d61baeb222126686b4c87f09e091321c61766b'
