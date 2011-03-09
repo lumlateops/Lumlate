b79050e8804bac86b611b6fdbf4a05fef6713a0d
Digvijay Lamba <lumlateops@gmail.com>
2011-03-09 05:29:23 +0100

    tabs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+jQuery.ajaxSetup({ 'beforeSend': function(xhr)
+{xhr.setRequestHeader("Accept", "text/javascript")} });
+
+$(document).ready(function(){
+	$( "#tabs" ).tabs({
+		ajaxOptions: {
+			error: function( xhr, status, index, anchor ) {
+				$( anchor.hash ).html(
+					"Couldn't load this tab. We'll try to fix this as soon as possible. ");
+			}
+		}
+	});
+});
