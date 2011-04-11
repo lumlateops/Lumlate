61a55aec657f506821cbdcbee79d329cf750277e
RuslanHamidullin <ruslan.hamidullin@flatsoft.com>
2011-04-12 02:06:24 +0400

    FOR DEALS: fixed stars ui issue between ajax queries

--- app/views/deals/deals_by_expiry_date.js
+++ app/views/deals/deals_by_expiry_date.js
@@ -1,2 +1,5 @@
 $(".deals_list").html("<%= escape_javascript(render(:partial => 'form', :collection => @deal_emails, :as => :deal_email)) %>");
+$('.rating_stars').stars({
+  inputType: "select"
+});
 
